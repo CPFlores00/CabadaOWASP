9e427f910d28ae05b4982c093862fef22d36a425
Carlos Flores <carlos_raul_98@outlook.com>
2019-04-18 00:25:29 -0700

    Added a api

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,52 @@
 import * as express from 'express';
 import { Application } from 'express';
-//import * as fs from 'fs';
-// import * as https from 'https';
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as https from 'https';
+import * as csrf  from 'csurf';
 import * as nodemailer from 'nodemailer';
 import * as bodyParser from 'body-parser';
+import * as mongoose from 'mongoose';
+import * as helmet from 'helmet';
 
 
 const app: Application = express();
+dotenv.config();
+app.use(helmet());
+// Remove default x-powered-by response header
+// -- app.disable('x-powered-by');
+// Prevent opening page in frame or iframe to protect from clickjacking
+// -- app.use(helmet.frameguard({ action: 'sameorigin' }));
+// Prevents browser from caching and storing page
+// -- app.use(helmet.noCache());
+// Allow loading resources only from white-listed domains
+// -- app.use(helmet.contentSecurityPolicy());
+// Allow communication only on HTTPS
+// -- app.use(helmet.hsts());
+// Enable XSS filter in IE (On by default)
+// -- app.use(helmet.ieNoOpen());
+// -- app.use(helmet.xssFilter({ setOnOldIE: true }));
+// Forces browser to only use the Content-Type set in the response header instead of sniffing or guessing it
+// -- app.use(helmet.noSniff());
+
+// Enable Express csrf protection
+app.use(csrf());
+// Make csrf token available in templates
+app.use((req, res, next) => {
+  res.locals.csrftoken = req.csrfToken();
+  next();
+});
+
+/*
+const httpsOptions = {
+  key: fs.readFileSync(path.resolve(__dirname, 'key')),
+  cert: fs.readFileSync(path.resolve(__dirname, 'crt'))
+};
+*/
+
+const db: string = 'mongodb://admin:ilovebacon03@ds139956.mlab.com:39956/cabada-project';
+mongoose.connect(db);
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
@@ -30,14 +70,16 @@ app.use(function (req, res, next) {
   next();
 });
 
-/*const commandLineArgs = require('command-line-args');
-
+/*
+import * as cla from 'command-line-args';
 const optionDefinitions = [
   { name: 'secure', type: Boolean, defaultOption: true },
 ];
+const options = cla(optionDefinitions);
+*/
 
-const options = commandLineArgs(optionDefinitions);*/
+console.log(app.locals);
 
-const httpsServer = app.listen(9000, () => {
-  console.log("HTTP Server Running");
+const httpsServer = app.listen(process.env.PORT_SERVER, () => {
+  console.log(`HTTP Server Running on ${ process.env.HOST_URL }`);
 });
